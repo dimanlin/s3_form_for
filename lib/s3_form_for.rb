bb953e66d581a56ca78e85b9c7bbcd87277bbcb7
Dmitry Khromov <devandart@ya.ru>
2015-04-10 12:10:38 +0300

    init

---
+++ lib/s3_form_for.rb
@@ -0,0 +1,13 @@
+require 's3_form_for/version'
+require 'jquery-fileupload-rails' if defined?(Rails)
+
+require 'base64'
+require 'openssl'
+require 'digest/sha1'
+
+require 's3_form_for/config_aws'
+require 's3_form_for/form_helper'
+require 's3_form_for/engine' if defined?(Rails)
+require 's3_form_for/railtie' if defined?(Rails)
+
+ActionView::Base.send(:include, S3DirectUpload::UploadHelper) if defined?(ActionView::Base)
